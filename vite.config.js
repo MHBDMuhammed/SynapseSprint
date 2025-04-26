df6ba046da666cbb37fae9941b7d1f7574e21adb
MHBDMuhammed <mhbdmuhammed@gmail.com>
2025-04-26 16:44:41 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: false,
-    minify: 'terser',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+    emptyOutDir: true
+  }
 })
